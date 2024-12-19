4b56c56e671f864751cccd0e25f97a08595a1650
GZolovick <george.zolovick@nps.edu>
2024-12-18 17:51:18 -0800

    Update Payload.py

--- Payload/Payload.py
+++ Payload/Payload.py
@@ -1,14 +1,15 @@
 # %% Import Libraries----------------------------------------------------------
 
-import pandas as pd                # used to import raw RGB files
-import numpy as np                 # used to stack/combine RGB files
-import matplotlib.pyplot as plt    # used for vizualizations
+import pandas as pd  # used to import raw RGB files
+import numpy as np  # used to stack/combine RGB files
+import matplotlib.pyplot as plt  # used for visualizations
 
 # %% Task-1: Load Raw RGB Input Files------------------------------------------
 
 red_csv = pd.read_csv("red.csv").values
 green_csv = pd.read_csv("green.csv").values
 blue_csv = pd.read_csv("blue.csv").values
+
 
 # %% Task-2: Verify RGB Band Dimensions----------------------------------------
 
@@ -17,18 +18,22 @@ def dimension_verification(red, green, blue):
     if red.shape != blue.shape or red.shape != green.shape:
         False, print("ERROR: The provided RGB files do not have the same size.")
     else:
-        True
-        
+        #True, print("Dimensions are equal")
+        verification = True
+        return (verification)
+
+
 # %% Task-3: Combine RGB Bands-------------------------------------------------
 
 # test if dimensions of inputs are equal, then combines into one RGB image
 def combine_RGB(red, green, blue):
     verification = dimension_verification(red, green, blue)
-    if verification == True:
+    if  verification == True:
+        #print("Dimensions are verified")
         RGB_image = np.stack((red, green, blue), axis=2).astype(np.uint8)
         return RGB_image
     else:
-         print("There is a combination or verification error")
+        print("There is a combination or verification error")
 
 '''
 When run, throws the following error:
@@ -46,12 +51,16 @@ def display_RGB_image(RGB_image):
     plt.show()
 
 # standalone test of display_RGB_image
+# display_RGB_image(blue_csv)                  # Visualize a single color band
 display_RGB_image(combine_RGB(red_csv, green_csv, blue_csv))
 
 # %% CheckPlus-1: Convert to Reflectance---------------------------------------
 
 
-# %% CheckPlus-2: Rescale Reflactance to 8-Bit---------------------------------
+# %% CheckPlus-2: Rescale Reflectance to 8-Bit---------------------------------
+
+
+# %% CheckPlus-3: Save the output image to a file------------------------------
 
 
 # %%---------------------------------------------------------------------------
